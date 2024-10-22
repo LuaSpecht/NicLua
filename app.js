1b041ffe26c682c3b1eb21187a377e702992e4cd
Luana Specht <spechtdeveloper@gmail.com>
2024-10-21 22:05:50 -0300

    build-project

---
+++ app.js
@@ -0,0 +1,11 @@
+const list = []
+
+console.log(Object.values(list))
+
+let lista = Object.values(list)
+console.log(lista.length)
+
+if(lista[0] == undefined || lista.length == 2) {
+  console.log('vazio')
+}
+console.log(undefined == null)
